dae7923faf50c0264500497c2c66c4560be2d9c6
+ <+>
2024-02-01 15:34:04 +0300

    Completed work

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,9 +14,10 @@ function Form(props) {
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+      <input type="text" onChange={handleFirstNameChange} value={firstName}/>
+      <input type="text" onChange={handleLastNameChange} value={lastName}/>
+      <button type="submit">SUBMIT</button>
+
     </form>
   );
 }
